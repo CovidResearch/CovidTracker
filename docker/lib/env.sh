e793b51914c94064d0bba414a9801a618b284bc6
Theodore R. Smith <theodore@phpexperts.pro>
2020-03-10 19:38:48 -0500

    Compatibility fix with the latest docker-php-stack.

--- docker/lib/env.sh
+++ docker/lib/env.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-export PROJECT_NAME=phpexpertslepp
+export PROJECT_NAME=covidtracker
 export REDIS_IMAGE=redis:3.2.6
-export REDIS_PASSWORD="Wqosrzcp-IHC5v!"
+export REDIS_PASSWORD="1223456"
